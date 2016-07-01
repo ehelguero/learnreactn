26fab7ad95e91f7377299d2f1bbaa0667bf07836
loupgarou81 <leloupgarou81@gmail.com>
2016-07-01 01:24:05 +0100

    Added image and styles

--- DemoRN.js
+++ DemoRN.js
@@ -5,6 +5,7 @@
 
 import React, { Component } from 'react';
 import {
+ Image,
  StyleSheet,
  Text,
  TextInput,
@@ -32,31 +33,40 @@ class demorn extends Component {
 
  render() {
    return (
-     <View style={styles.container}>
-       <Text style={styles.welcome}>
-         You input {this.state.zip}
-       </Text>
-       <Forecast
-         main={this.state.forecast.main}
-         description={this.state.forecast.description}
-         temp={this.state.forecast.temp}
-       />
-       <TextInput
-         style={styles.input}
-         returnKeyType="go"
-         onSubmitEditing={this._handleTextChange.bind(this)}
-        />
-     </View>
+       <View style={styles.container}>
+         <Image source={require('./globe.jpg')}
+           resizeMode='cover'
+           style={styles.backdrop} >
+          <View style={styles.overlay}>
+            <Text style={styles.welcome}>
+              You input {this.state.zip}
+            </Text>
+            <Forecast
+              main={this.state.forecast.main}
+              description={this.state.forecast.description}
+              temp={this.state.forecast.temp}
+            />
+            <TextInput
+              style={styles.input}
+              returnKeyType="go"
+              onSubmitEditing={this._handleTextChange.bind(this)}
+             />
+          </View>
+        </Image>
+       </View>
    );
  }
 }
 
 const styles = StyleSheet.create({
+  backdrop: {
+    flex: 1,
+    flexDirection: 'column',
+  },
  container: {
    flex: 1,
-   justifyContent: 'center',
    alignItems: 'center',
-   backgroundColor: '#333',
+   paddingTop: 30,
  },
  welcome: {
    fontSize: 20,
@@ -67,6 +77,13 @@ const styles = StyleSheet.create({
    fontSize: 20,
    borderWidth: 2,
    height: 40,
+ },
+ overlay: {
+   paddingTop: 5,
+   backgroundColor: '#000',
+   opacity: 0.5,
+   flexDirection: 'column',
+   alignItems: 'center',
  }
 });
 
